e27804d3eec6a2b397125fb6fb5de37771492eb0
Graham Joonsar <gtj162@icloud.com>
2023-11-29 07:46:17 -0500

    Adding the basic classes

--- main.py
+++ main.py
@@ -16,16 +16,15 @@ pygame.display.set_caption("Snowball Fight!")
 pygame.font.init()
 myfont = pygame.font.SysFont('Comic Sans MS', 30)
 
+num_players = 8
 
 # Initializing the joysticks, there should be 8 for 8 players
 pygame.joystick.init()
-if pygame.joystick.get_count() != 8:
+if pygame.joystick.get_count() != num_players:
     print("ERROR: Incorrect number of joysticks")
     print("There are only " + str(pygame.joystick.get_count()) + " joysticks.")
 
 joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
-
-num_players = 8
 
 # List of 8 unique and distinct colors, good for identifying the players
 colors = [(255, 0, 0, 255), 
@@ -37,6 +36,30 @@ colors = [(255, 0, 0, 255),
           (255, 100, 100, 255), 
           (165, 42, 42, 255)
 ]
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y, side, number, img):
+        # Pygame and image stuff
+        pygame.sprite.Sprite.__init__(self)
+
+        # Positional and game stuff
+        self.x = x
+        self.y = y
+        self.side = side
+        self.number = number
+
+class Snowball(pygame.sprite.Sprite):
+    def __init__(self, x, y, side):
+        self.x = x
+        self.y = y
+        self.side = side
+
+class Barrier(pygame.sprite.Sprite):
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
 
 # Main Loop (Press ESC to force quit)
 running = True
